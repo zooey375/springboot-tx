d9220d352c0cd231a464e294c543c8b467263e90
zooey <yu.ting375@gmail.com>
2025-05-28 12:21:53 +0800

    20250528_1221

--- src/main/java/com/example/demo/service/impl/BuyServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BuyServiceImpl.java
@@ -2,13 +2,14 @@ package com.example.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.exception.InsufficientAmountException;
 import com.example.demo.repository.BookRepository;
 import com.example.demo.service.BookService;
 import com.example.demo.service.BuyService;
-
-import jakarta.transaction.Transactional;
 
 @Service // 交易服務
 public class BuyServiceImpl implements BuyService {
@@ -19,8 +20,10 @@ public class BuyServiceImpl implements BuyService {
 	// RuntimeException 預設會回滾, 可以透過 dontRollbackOn 來改變
 	// Exception 預設不會回滾, 可以透過 rollbackOn 來定義
 	@Transactional(
-			rollbackOn = {InsufficientAmountException.class},
-			dontRollbackOn = {RuntimeException.class}
+			propagation = Propagation.REQUIRED, // 預設
+			isolation = Isolation.DEFAULT, // 預設:使用資料庫預設。  隔離層級:DEFAULT
+			rollbackFor = {InsufficientAmountException.class},
+			noRollbackFor = {RuntimeException.class}
 	)
 	@Override
 	public void buyOneBook(String username, Integer bookId) throws InsufficientAmountException {
